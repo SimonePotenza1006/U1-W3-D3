bfeb0722d8aea5393f0d5361eb5f812505f51ffc
SimonePotenza1006 <simonepotenzatr@gmail.com>
2023-06-07 16:07:32 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+//text decoration
+
+
+let taskForm = document.getElementById("form")
+taskForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const taskInputField = document.getElementById("tasks")
+    const savedTasks = taskInputField.value
+    let newTask = document.createElement('div')
+    newTask.classList.add("singleTask")
+    newTask.innerHTML = `
+    <p class="doneTask">${savedTasks}</p>
+    <button>ELIMINA</button>
+    `
+    const savedTasksDiv = document.getElementById("saved-tasks")
+    savedTasksDiv.appendChild(newTask)
+
+    const allTheDeleteButtons = document.querySelectorAll(".singleTask button")
+    allTheDeleteButtons.forEach((button) =>{
+        button.addEventListener("click", function(e) {
+            let buttonIJustClicked = e.target
+            let taskToRemove = buttonIJustClicked.parentElement
+            taskToRemove.remove()
+
+        })
+    })
+    taskInputField.value =""
+})
